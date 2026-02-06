a16d097f2658fdae79148b6c4b652598d41b4cdd
Umayantha <umayantha@globaltechdevelop.com>
2026-02-06 16:44:20 +0530

    spa authentication signing users in

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: [
+            'auth/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
